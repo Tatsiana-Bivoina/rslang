a920271feb2857184ac8a97f9b3a13533af7c988
vostavhy <vostavhy@gmail.com>
2022-02-17 22:03:57 +0300

    feat: add some changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -25,7 +25,6 @@ export class App {
       <span class="menu login">${Menu.login}</span>
     `;
 
-
     // отрисовка футера
     const footer: HTMLDivElement = document.createElement('footer') as HTMLDivElement;
     footer.classList.add('footer');
